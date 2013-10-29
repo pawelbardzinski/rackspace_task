d83caa810d5c4d8f2c369adf566e9e9502411292
Pawel Bardzinski <pawelbardzinski@Pawels-MacBook-Pro.local>
2013-10-29 17:45:07 -0400

    Starting work!

---
+++ VetApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VetApp::Application.config.secret_key_base = 'fd49d60e929de1086880084eacc1afd21457b3a82e35badc37db318c8bfb81f7f1797980336a8885229cbc05223761fbbca9184522d8525ab4960aa27825b514'
